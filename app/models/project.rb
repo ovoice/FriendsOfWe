1ea293c6dc708343518b188603592a2da1ebcc8f
rio517 <mario@railsdog.com>
2009-11-12 14:00:27 -0500

    First pass at views - forgot to commit  files

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,8 @@
 class Project < ActiveRecord::Base
   has_many :assets, :as => :assetable
+  has_many :links, :as => :assetable
+  has_many :documents, :as => :assetable
+  has_many :photos, :as => :assetable
   has_many :commitments
   has_many :volunteers, :through => :commitments, :source => :user  #, :condition => 'state = "confirmed"'
   
