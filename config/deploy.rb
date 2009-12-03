b7adcd49b61b762ed10f88d9f4542ffdbc09fdc7
rio517 <mario@railsdog.com>
2009-12-02 23:32:54 -0500

    Updating deploy recipie

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,21 +9,20 @@ set :repository,          "git@github.com:ovoice/FriendsOfWe.git"
 set :deploy_via,          :remote_cache
 set :real_revision,       lambda { source.query_revision(revision) { |cmd| capture(cmd) } } # This will execute the Git revision parsing on the *remote* server rather than locally
 
-set :dbuser,              "FriendsOfWe  "
-
+set :dbuser,              "root"
 
 ssh_options[:paranoid] = false # comment out if it gives you trouble. newest net/ssh needs this set.
 
 task :production do
   set :password,            "8uvedRat"
   set :branch,              "master"
-  set :staging_database,    "friendsofwe_production"
-  set :staging_dbhost,      "localhost"
+  set :production_database, "friendsofwe_production"
+  set :production_dbhost,   "localhost"
   set :dbpass,              "lkL823las3e"
   set :rails_env,           "production"
-  role :web, "208.88.124.43"
-  role :app, "208.88.124.43"
-  role :db , "208.88.124.43", :primary => true
+  role :web, "208.88.124.160"
+  role :app, "208.88.124.160"
+  role :db , "208.88.124.160", :primary => true
 end
 
 after "deploy", "deploy:cleanup"
