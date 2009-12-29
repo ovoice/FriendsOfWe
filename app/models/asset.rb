8fc256af9503504111816b237d71cfc28bcaa12e
rio517 <mario@railsdog.com>
2009-12-29 01:10:09 -0500

    making Asset.description attr_accessible

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -1,4 +1,4 @@
 class Asset < ActiveRecord::Base
-  belongs_to :assetable
-  attr_accessible :name, :url, :attachment_file_name, :attachment_content_type, :attachment_file_size, :attachment_updated_at, :state
+  belongs_to :assetable, :polymorphic => true
+  attr_accessible :name, :url, :description, :attachment_file_name, :attachment_content_type, :attachment_file_size, :attachment_updated_at, :state
 end
