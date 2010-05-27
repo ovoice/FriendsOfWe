325479ed172b3a077017f0e4496f248643605960
Mario Olivio Flores <rio517@gmail.com>
2010-05-27 02:51:23 -0400

    minor fix for latest formtastic

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -8,7 +8,7 @@ class PasswordResetsController < ApplicationController
   end
 
   def create
-    @user = User.find_by_email(params[:email])
+    @user = User.find_by_email(params[:user][:email])
     if @user
       @user.deliver_password_reset_instructions!
       flash[:notice] = "Instructions to reset your password have been emailed to you. Please check your email."
