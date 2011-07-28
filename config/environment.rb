944b1f4a90aa76be4f4b21f0a661bec1b7618856
rio <rio517@gmail.com>
2011-07-27 23:05:30 -0300

    adding data from current host

--- config/environment.rb
+++ config/environment.rb
@@ -31,6 +31,7 @@ Rails::Initializer.run do |config|
   config.gem "populator"
   config.gem "faker"
   config.gem "feed-normalizer"
+  config.gem "yaml_db"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
